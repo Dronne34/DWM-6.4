4287b65b99ed1de79be8e237765cd8350f974873
Dronne34 <ozy23as@gmail.com>
2023-08-11 19:48:42 +0200

    dwm-6.4 no change

--- config.def.h
+++ config.def.h
@@ -2,15 +2,15 @@
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const int startwithgaps[]    = { 8 };	/* 1 means gaps are used by default, this can be customized for each tag */
-static const unsigned int gappx[]   = { 8 };   /* default gap between windows in pixels, this can be customized for each tag */
+static const int startwithgaps[]    = { 1 };	/* 1 means gaps are used by default, this can be customized for each tag */
+static const unsigned int gappx[]   = { 10 };   /* default gap between windows in pixels, this can be customized for each tag */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const Bool viewontag         = True;     /* Switch view on tag switch */
 static const int viewonrulestag 	= 1;		 /* 1 means when open applications view will move to tags defined in rules*/
-static const char *fonts[]          = { "monospace: size=10" };
-static const char dmenufont[]       = "monospace: size=10";
+static const char *fonts[]          = { "Hack:size=10" };
+static const char dmenufont[]       = "Hack:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -20,7 +20,7 @@ static const char col_urgborder[]   = "#ff0000";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_gray2,  col_cyan  },
+	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
 	[SchemeUrg]  = { col_gray4, col_cyan,  col_urgborder  },
 };
 
@@ -83,7 +83,6 @@ static const Layout layouts[] = {
 static const char *dmenucmd[]      =   { "dmenu_run",        NULL };
 static const char *termcmd[]       =   { "alacritty",        NULL };
 static const char *browsercmd[]    =   { "firefox",          NULL };
-// static const char *ncmpcppcmd[]    =   { "dm-ncmpcpp",   NULL };
 
 
 static const Key keys[] = {
--- config.h
+++ config.h
@@ -9,8 +9,8 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const Bool viewontag         = True;     /* Switch view on tag switch */
 static const int viewonrulestag 	= 1;		 /* 1 means when open applications view will move to tags defined in rules*/
-static const char *fonts[]          = { "monospace: size=10" };
-static const char dmenufont[]       = "monospace: size=10";
+static const char *fonts[]          = { "Hack:size=10" };
+static const char dmenufont[]       = "Hack:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -20,7 +20,7 @@ static const char col_urgborder[]   = "#ff0000";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_gray2,  col_cyan  },
+	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
 	[SchemeUrg]  = { col_gray4, col_cyan,  col_urgborder  },
 };
 
@@ -83,7 +83,6 @@ static const Layout layouts[] = {
 static const char *dmenucmd[]      =   { "dmenu_run",        NULL };
 static const char *termcmd[]       =   { "alacritty",        NULL };
 static const char *browsercmd[]    =   { "firefox",          NULL };
-// static const char *ncmpcppcmd[]    =   { "dm-ncmpcpp",   NULL };
 
 
 static const Key keys[] = {
