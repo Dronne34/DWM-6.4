b8e2a81c70ca595a520cdd7e823a1eef45c831e9
Dronne34 <ozy23as@gmail.com>
2023-10-07 05:32:14 +0300

    font size change to 13 px.

--- config.def.h
+++ config.def.h
@@ -9,8 +9,8 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const Bool viewontag         = True;     /* Switch view on tag switch */
 static const int viewonrulestag 	= 1;		 /* 1 means when open applications view will move to tags defined in rules*/
-static const char *fonts[]          = { "Hack:size=10" };
-static const char dmenufont[]       = "Hack:size=10";
+static const char *fonts[]          = {"Iosevka Nerd Font:style=Regular:size=10"};
+static const char dmenufont[]       = {"FantasqueSansMono Nerd Font:size=13"};
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
