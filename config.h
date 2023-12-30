e77e4ba0923f63457422777de827bba99b9468be
Dronne34 <ozy23as@gmail.com>
2023-12-30 05:43:46 +0200

    update color green add!

--- config.h
+++ config.h
@@ -12,18 +12,20 @@ static const char *fonts[]          = {"Mononoki Nerd Font:style=Regular:size=12
 static const char dmenufont[]       = {"FantasqueSansMono Nerd Font:size=12"};
 static const Bool viewontag         = True;     /* Switch view on tag switch */
 /* color */
+
+static const char col_gray[]        = "#2A303A";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
 static const char col_cyan1[]       = "#C33445";
-static const char col_cyan2[]       = "#FFA800";
+static const char col_cyan2[]       = "#37A95A";
 
 static const char *colors[][3]      = {
     /*               fg         bg         border   */
-    [SchemeNorm] = { col_gray3, col_gray1, col_cyan },
-    [SchemeSel]  = { col_gray4, col_cyan,  col_cyan2  },
+    [SchemeNorm] = { col_gray4, col_gray, col_gray2 },
+    [SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
 };
 
 /* tagging */
