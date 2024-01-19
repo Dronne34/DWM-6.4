6d1498baeb78e697182a25eaf3850b9d42851346
Dronne34 <ozy23as@gmail.com>
2024-01-19 06:25:13 +0200

    update config

--- config.def.h
+++ config.def.h
@@ -94,7 +94,7 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 
 	
 static const char *dmenucmd[]   = { "dmenu_run_history", "-i", "-l", "15", NULL };
-static const char *browsercmd[] = { "chromium",                    NULL };
+static const char *browsercmd[] = { "chromium",                       NULL };
 static const char *termfile[]   = { "xterm", "-e", "ranger",          NULL };
 static const char *termedit[]   = { "xterm", "-e", "vim",             NULL };
 
