2aa72d17fbb41f080cc0a48430e4b3c01dffe0a1
Dronne34 <ozy23as@gmail.com>
2024-02-11 19:37:26 +0200

    update font hack

--- config.def.h
+++ config.def.h
@@ -96,8 +96,8 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 	
 static const char *dmenucmd[]   = { "dmenu_run_history", "-i", "-l", "15", NULL };
 static const char *browsercmd[] = { "chromium",                       NULL };
-static const char *termfile[]   = { "st", "-f", "DejaVuSansMono:style=Regular:size=13", "-e", "ranger",             NULL };
-static const char *termedit[]   = { "st", "-f", "DejaVuSansMono:style=Regular:size=13", "-e",  "vim",               NULL };
+static const char *termfile[]   = { "st", "-e", "ranger",             NULL };
+static const char *termedit[]   = { "st", "-e",  "vim",               NULL };
 
 static const char *termcmd[]    = { "st",                                                   NULL };
 static const char *xranger[]    = { "st", "-n", "x_ranger", "-g", "120x40", "-e", "ranger", NULL };
